80cad0c348c9224121a66239e7a174e0aa9fd157
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2018-07-16 02:25:54 +0600

    Add files via upload

---
+++ UVA-13278 - Angry Birds Transformers.cpp
@@ -0,0 +1,89 @@
+/**
+Bismillahir Rahmanir Rahim
+MD. Iqbal Hossain Bappy
+Dept. of CSE, CoU
+**/
+
+#include<bits/stdc++.h>
+#include<queue>
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scln(x)                 sc("%lld",&(x))
+#define pf                      printf
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483647;
+#define longlimit               9223372036854775808
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+
+///------------------Graph Moves-------------------
+/// const int fx[] = {+1,-1,+0,+0};
+/// const int fy[] = {+0,+0,+1,-1};
+/// const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+/// const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///King's move
+/// const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///Knight's move
+/// const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///Knight's move
+
+int x[10005],y[10005],sum[10005];
+int main()
+{
+    int n,t,T,cnt=0,p,q;
+    while(1)
+    {
+        cnt=0;
+        ms(x,0);
+        ms(y,0);
+        ms(sum,0);
+        scin(n);
+        if(n == 0)
+            return 0;
+        for(int i=1;i<=n;i++)
+        {
+            scin(x[i]);
+            scin(y[i]);
+            p=x[i]-y[i];
+            q=x[i]+y[i];
+            if(p<0)
+                p=0;
+            if(q>10000)
+                q=10000;
+            sum[p]+=1;
+            sum[q+1]-=1;
+        }
+        for(int i=1;i<=10000;i++)
+            sum[i]+=sum[i-1];
+        for(int i=0;i<=10000;i++)
+            cnt=max(cnt,sum[i]);
+        pf("%d\n",cnt);
+    }
+    return 0;
+}
+
